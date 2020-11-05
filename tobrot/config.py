ce40b09f714ce0707add81e3f8f910cbaf8900b2
manimaran-1 <68263470+manimaran-1@users.noreply.github.com>
2020-11-05 21:57:41 +0530

    Update and rename g_config.py to config.py

---
+++ tobrot/config.py
@@ -0,0 +1,13 @@
+from tobrot.sample_config import Config
+#Fill your all data, read readme for reference
+
+class Config(Config):
+	TG_BOT_TOKEN= "1379911592:AAFkjpVY5PaOvyj6TtWHEDXTvjRxWLmQAdw"
+	APP_ID = 1674832
+	API_HASH = "ae3433b211f7b86c3ee038997e4e1ad0"
+	OWNER_ID = "786418598" #ID of bot owner
+	AUTH_CHANNEL = [-1001302967369]
+	DESTINATION_FOLDER = "0AH_ZEIqAXEc7Uk9PVA" #Name of your folder read readme
+	RCLONE_CONFIG = """type = drive\nscope = drive\nroot_folder_id = 1zRhfAbKOWNrg2qh-VQcquRPGvWierrDG\ntoken = {"access_token":"ya29.a0AfH6SMCFFXK-Oex6lWBkx97c062enChMLpDOQ1fc8imqIhlBbDxgr4sGQO0fcXQMEQPn6iDaJVfEm1ezLb811j6ymm6VPm4gJPW-ledXEjJHbFh6Acmg2q7Jw3kMob6qC3SWiPQFnyy_ZiEv7xBQxntbVkpvKIpgp0JOiQ","token_type":"Bearer","refresh_token":"1//0gZXP7EYlLy7bCgYIARAAGBASNwF-L9IrlM2o0SdqKEMRPDw7UmuZMnK8cXqVQ3CP0TQxU0JyQrK0pMG741XD44tKDSr_DeU5exg","expiry":"2020-10-06T08:23:10.6304723+05:30"}"""
+	INDEX_LINK = "https://server3-washingtondc.mohanraj.workers.dev/0:/0AH_ZEIqAXEc7Uk9PVA/0AH_ZEIqAXEc7Uk9PVA"
+	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
